cae92aba50fe464e68cbe5256bf40bb10ab4e34c
Nick Earl <nick.r.earl@gmail.com>
2022-05-07 11:36:04 -0400

    created initialize page function, dispay current date and time at top of page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,23 @@
 var timeBlocks = $('#time-block')
+var dayDisplay = $('#currentDay')
 
 
-var hourCount = 9;
+
 
 var addTimeBlocks = function(){
+    var hourCount = 9;
     for (i = 0; i < hourCount; i++) {
         var currentHour = moment().set('hour', 9 + i)
         $("<div class='col-12 col-lg border border-primary p-4'>")
         .text(moment(currentHour).format("ha"))
         .appendTo(timeBlocks)
-        console.log(currentHour);
     }
 }
 
-addTimeBlocks()
+var initializePage = function (){
+    dayDisplay.text(moment().format('MMMM Do YYYY, h:mm a'))
+
+    addTimeBlocks()
+}
+
+initializePage()
