83848b40165269fa152d8005861bf0f247052df4
Nick Earl <nick.r.earl@gmail.com>
2022-05-08 01:25:10 -0400

    added persistance with local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -39,45 +39,139 @@ var pageDisplay = function(){
 
         saveButton.appendTo(hourBlockDiv)
 
-
-
         hourBlockDiv.appendTo(timeBlocks)
 
     }
 
-    timeBlocks.on("click", ".task-field", function(){
-        $(this).text("")
+    getTasks()
+}
 
-        var text = $(this)
-        .text()
-        .trim();
-    
-        var textInput = $("<textarea>")
-        .addClass("task-input")
-        .val(text);
-    
-        textInput.appendTo($(this));
-        
-        textInput.trigger("focus");
-    })
+var getTasks = function(){
+    var firstTask = localStorage.getItem("task1")
+    var secondTask = localStorage.getItem("task2")
+    var thirdTask = localStorage.getItem("task3")
+    var fourthTask = localStorage.getItem("task4")
+    var fifthTask = localStorage.getItem("task5")
+    var sixthTask = localStorage.getItem("task6")
+    var seventhTask = localStorage.getItem("task7")
+    var eighthTask = localStorage.getItem("task8")
+    var ninthTask = localStorage.getItem("task9")
 
-    timeBlocks.on("blur", ".task-input", function(){
-        var text = $(".task-input")
-        .val()
-        .trim()
+$('[data-index="0"]')
+    .text(firstTask)
 
 
-        $(".task-input").closest($(".task-field")).text(text)
+$('[data-index="1"]')
+    .text(secondTask)
 
-        $(".task-input").remove()
 
-    })
+$('[data-index="2"]')
+    .text(thirdTask)
 
-    timeBlocks.on("click", ".saveBtn", function(){
 
-    })
+$('[data-index="3"]')
+    .text(fourthTask)
+
+
+$('[data-index="4"]')
+    .text(fifthTask)
+
+
+$('[data-index="5"]')
+    .text(sixthTask)
+
+
+$('[data-index="6"]')
+    .text(seventhTask)
+
+
+$('[data-index="7"]')
+    .text(eighthTask)
+
+
+$('[data-index="8"]')
+    .text(ninthTask)
+
 
 }
+
+timeBlocks.on("click", ".task-field", function(){
+    $(this).text("")
+
+    var text = $(this)
+    .text()
+    .trim();
+
+    var textInput = $("<textarea>")
+    .addClass("task-input")
+    .val(text);
+
+    textInput.appendTo($(this));
+    
+    textInput.trigger("focus");
+})
+
+timeBlocks.on("blur", ".task-input", function(){
+    var text = $(".task-input")
+    .val()
+    .trim()
+
+
+    $(".task-input").closest($(".task-field")).text(text)
+
+    $(".task-input").remove()
+
+})
+
+
+timeBlocks.on("click", ".saveBtn", function(){
+    var firstTaskField = $('[data-index="0"]')
+    .text()
+    .trim()
+
+    var secondTaskField = $('[data-index="1"]')
+    .text()
+    .trim()
+
+    var thirdTaskField = $('[data-index="2"]')
+    .text()
+    .trim()
+
+    var fourthTaskField = $('[data-index="3"]')
+    .text()
+    .trim()
+
+    var fifthTaskField = $('[data-index="4"]')
+    .text()
+    .trim()
+
+    var sixthTaskField = $('[data-index="5"]')
+    .text()
+    .trim()
+
+    var seventhTaskField = $('[data-index="6"]')
+    .text()
+    .trim()
+
+    var eighthTaskField = $('[data-index="7"]')
+    .text()
+    .trim()
+
+    var ninthTaskField = $('[data-index="8"]')
+    .text()
+    .trim()
+
+    localStorage.setItem("task1", firstTaskField);
+    localStorage.setItem("task2", secondTaskField);
+    localStorage.setItem("task3", thirdTaskField);
+    localStorage.setItem("task4", fourthTaskField);
+    localStorage.setItem("task5", fifthTaskField);
+    localStorage.setItem("task6", sixthTaskField);
+    localStorage.setItem("task7", seventhTaskField);
+    localStorage.setItem("task8", eighthTaskField);
+    localStorage.setItem("task9", ninthTaskField);
+
+})
 
 
 dateDisplay()
